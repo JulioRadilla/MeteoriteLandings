2d6fd887e3a3b10795ff68d1e40e0f16de8d84b2
Julio Radilla <itsdigitaljulio@github.com>
2023-07-21 23:27:15 -0500

    Worked on the backend to make the search to work with mixed cases , mutliple words with spaces and dashes

--- controllers/home.js
+++ controllers/home.js
@@ -1,25 +1,50 @@
 const { default: axios } = require("axios")
 
 module.exports = {
-    getHomepage: (req,res) => {
-        res.render('index')
+    getHomepage: async (req,res) => {
+        const response = await axios.get(`https://data.nasa.gov/resource/gh4g-9sfh.json`)
+            const result = response.data
+            console.log(result)
+            res.render('index', {
+                result
+            })
     },
     getMeteoriteInfo: async (req,res) => {
         try{
-            const searchTerms = req.query
-
+            let { searchTerms } = req.query
+            
+            console.log(searchTerms)
             let filterParams
-            if(searchTerms !== ''){
-                filterParams = `name=${searchTerms}`
-            } else {
-                filterParams = `Please insert search term name`
-            }
-
-            let response = await axios.get(`https://data.nasa.gov/resource/gh4g-9sfh.json?${filterParams}`);
-            let datas = await response
-            res.render('index', {
-                datas
-            })
+            if (searchTerms !== '') {
+                let arr;
+                if (searchTerms.includes(' ')) {
+                  arr = searchTerms.split(' ');
+                  searchTerms = arr
+                    .map((item) => item[0].toUpperCase() + item.slice(1).toLowerCase())
+                    .join(' ');
+                } else if (searchTerms.includes('-')) {
+                  arr = searchTerms.split('-');
+                  searchTerms = arr
+                  .map((item) => item[0].toUpperCase() + item.slice(1).toLowerCase())
+                  .join('-');
+                } else {
+                  searchTerms = searchTerms[0].toUpperCase() + searchTerms.slice(1).toLowerCase();
+                }
+            
+                filterParams = `name=${searchTerms}`;
+              } else {
+                filterParams = '';
+              }
+            
+            console.log(filterParams)
+            axios.get(`https://data.nasa.gov/resource/gh4g-9sfh.json?${filterParams}`)
+            .then((response) => {
+                const datas = response.data
+                console.log(datas)                           
+                res.render('index', {
+                    datas
+                })
+            })   
         } catch(err){
             console.error(err)
         }
