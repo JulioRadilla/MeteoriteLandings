3e91474e0663643d66660f6725d73bea20c8a89d
Julio Radilla <itsdigitaljulio@github.com>
2023-07-27 21:43:37 -0500

    Finished backend code , made feautures  if api request takes long, if user enters a name that the api doesn't have, made features if there is errors, lastly if there isn't a data inside the attributes of meteorites it will display not recorded , will work on front end tomorrow

--- controllers/home.js
+++ controllers/home.js
@@ -8,6 +8,8 @@ module.exports = {
       try {
         const response = await axios.get(`https://data.nasa.gov/resource/gh4g-9sfh.json`);
         originalData = response.data; // Store the API response in the originalData variable
+
+       
         
         
       // Pagination logic
@@ -16,6 +18,17 @@ module.exports = {
       const startIndex = (page - 1) * itemsPerPage;
       const endIndex = startIndex + itemsPerPage;
       const paginatedData = originalData.slice(startIndex, endIndex);
+
+      // Handle empty search term
+      let { searchTerms } = req.query;
+      if (!searchTerms || searchTerms.trim() === '') {
+        console.log("Original Data:", originalData);
+        res.render('index', {
+          matchingElement: paginatedData, // Render the paginated data for the first 50 items
+          currentPage: page
+        });
+        return; // Exit the function early
+      }
 
         res.render('index', {
           matchingElement:  paginatedData,
@@ -29,16 +42,26 @@ module.exports = {
         
         try{
             let { searchTerms } = req.query
+
+            // Pagination logic
+            const page = parseInt(req.query.page) || 1; // Get the page number from the query parameter (default to 1 if not specified)
+            const itemsPerPage = 50;
+            const startIndex = (page - 1) * itemsPerPage;
+            const endIndex = startIndex + itemsPerPage;
+            const paginatedData = originalData.slice(startIndex, endIndex);
             
             if (!searchTerms || searchTerms.trim() === '') {
               console.log("Original Data:", originalData);
               res.render('index', {
-                  matchingElement: originalData
+                  matchingElement: paginatedData,
+                  currentPage: page
               });
               return; // Exit the function early
           }
 
-            const response = await axios.get(`https://data.nasa.gov/resource/gh4g-9sfh.json`)
+            const response = await axios.get(`https://data.nasa.gov/resource/gh4g-9sfh.json`, {
+              timeout: 5000 // Set the timeout to 5000 milliseconds (5 seconds) if api response takes too long
+            })
             const result = response.data                     
             const regexPattern = /[\s|-]/g;
             searchTerms = searchTerms.toLowerCase().replace(regexPattern, '')
@@ -48,11 +71,21 @@ module.exports = {
                 matchingElement.push(element)                                
               }             
             });
+
+            const paginatedMatchingElement = matchingElement.slice(startIndex, endIndex);
+
             res.render('index', {
-              matchingElement
+              matchingElement: paginatedMatchingElement,
+              currentPage: 1
             })
         } catch(err){
+          if (err.code === 'ECONNABORTED') {
+            // Handle timeout error
+            res.render('error', { message: 'The request took too long. Please try again later.' });
+          } else {
             console.error(err)
+            res.render('error', { message: 'An error occurred while processing your request.' });
+          }
         }
     }
 }
--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('h1').addEventListener('click', async ()=>{
-
-    let response = await fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
-    let data = await response.json()                        
-    console.log(data)
-})
-
